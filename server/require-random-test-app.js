61a6eb9e71c2ff1a8620868b8d54d0271d460733
SomeSN <nightlitequinox@gmail.com>
2018-05-05 21:58:01 -0700

    Ungenerifying module so it only does the functionality we need for our app.

--- server/require-random-test-app.js
+++ server/require-random-test-app.js
@@ -1,5 +1,4 @@
 const randomGenerator = require('./random-generation')
 
-console.log(randomGenerator.randomNumberGenerator())
 console.log(randomGenerator.randomCharGenerator())
 console.log(randomGenerator.randomStringGenerator())
