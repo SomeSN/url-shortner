de55b58be421cec27a649027d42461fc061af9c9
SomeSN <nightlitequinox@gmail.com>
2018-05-08 18:36:18 -0700

    Random shortURL code added to postForm in routes/index.js.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,15 @@
+const randomStringGen = require('../random-generation')
 const database = []
 
 module.exports = {
-        postForm(req, res) {
-        database.push(req.body)
-
-
-        res.status(200).send({
-          message : `Success! Your URL has been created: ${req.body.shortURL}`,
-        })
-        console.log(database)
-      }
+	postForm(req, res) {
+		if(req.body.shortURL === undefined || req.body.shortURL === null || req.body.shortURL === ''){
+			req.body.shortURL = randomStringGen.randomStringGenerator()
+		}
+		database.push(req.body)
+		res.status(200).send({
+			message : `Success! Your URL has been created: ${req.body.shortURL}`,
+		})
+		console.log(database)
+    },
 }
