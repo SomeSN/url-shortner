c7b848a81b4077978a51cfe9a82533432ab3376a
Noi <Noi@Nois-MacBook-Pro.local>
2018-05-08 19:44:06 -0700

    update and working well

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,6 +7,8 @@ module.exports = {
 
         res.status(200).send({
           message : `Success! Your URL has been created: ${req.body.shortURL}`,
+          shortURL: req.body.shortURL,
+          originalURL: req.body.originalURL
         })
         console.log(database)
       }
